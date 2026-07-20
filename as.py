53d5021c1af18edb777d1c301c5f91a2c1640d65
bitfoxgg-create <bitfoxgg@gmail.com>
2026-07-20 19:33:27 +0530

    Update as.py

--- as.py
+++ as.py
@@ -185,12 +185,9 @@ async def start(message: Message, state: FSMContext):
         "/cancel - Cancel current operation"
     )
     
-    entities = [
-        MessageEntity(type="custom_emoji", offset=0, length=2, custom_emoji_id="5377548235709619284"),
-        MessageEntity(type="custom_emoji", offset=28, length=2, custom_emoji_id="5287684458881756303"),
-    ]
     
-    await message.answer(text, entities=entities)
+    
+    await message.answer(text, entities=[emoji_entity])
 
 @dp.message(Command("cancel"))
 async def cancel(message: Message, state: FSMContext):
